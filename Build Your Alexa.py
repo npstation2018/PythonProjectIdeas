f23de554ff1824290719311f9f5d14abc98c417a
npstation2018 <89783844+npstation2018@users.noreply.github.com>
2021-09-27 13:45:19 -0400

    Add files via upload

---
+++ Build Your Alexa.py
@@ -0,0 +1,62 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.runAndWait()
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+    
+    
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+    
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('The current time is ' + time)
+        print(time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'hello' in command:
+        talk('Hi!')
+    elif 'how are you doing' in command:
+        talk('I am doing good! Thanks for asking!')
+    elif 'bye' in command:
+        quit
+    else:
+        talk('Please say the command again.')
+        
+
+while True:
+    run_alexa()
