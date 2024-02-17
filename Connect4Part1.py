190917c099a991bf813930fa5d7d233de66c0e2c
npstation2018 <89783844+npstation2018@users.noreply.github.com>
2024-02-17 18:28:07 -0500

    Add files via upload

---
+++ Connect4Part1.py
@@ -0,0 +1,28 @@
+import random
+
+print("Welcome to Connect Four")
+print("-----------------------")
+
+possibleLetters = ["A","B","C","D","E","F","G"]
+gameBoard = [["","","","","","",""], ["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""]]
+rows = 6
+cols = 7
+
+def printGameBoard():
+  print("\n     A    B    C    D    E    F    G  ", end="")
+  for x in range(rows):
+    print("\n   +----+----+----+----+----+----+----+")
+    print(x, " |", end="")
+    for y in range(cols):
+      if(gameBoard[x][y] == "🔵"):
+        print("",gameBoard[x][y], end=" |")
+      elif(gameBoard[x][y] == "🔴"):
+        print("", gameBoard[x][y], end=" |")
+      else:
+        print(" ", gameBoard[x][y], end="  |")
+  print("\n   +----+----+----+----+----+----+----+")
+
+def modifyTurn(spacePicked, turn):
+  gameBoard[spacePicked[0]][spacePicked[1]] = turn
+
+printGameBoard()
